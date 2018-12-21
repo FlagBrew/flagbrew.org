59ca46484e79c336d4bd0ddff26173dc86f7621d
Allen <allenlydiard@gmail.com>
2018-12-20 21:43:16 -0400

    added reload option to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/screen -dmS flagbrew.org /usr/bin/gunicorn -w 1 -b 0.0.0.0:4000 app:app
+/usr/bin/screen -dmS flagbrew.org /usr/bin/gunicorn -w 1 -b --reload 0.0.0.0:4000 app:app
