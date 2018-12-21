32070cf522aaa46c94a180ceab0e893592a0db1b
Allen <allenlydiard@gmail.com>
2018-12-20 21:21:56 -0400

    Basic Start

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
+app = Flask(__name__)
+socket = SocketIO(app)
+construction_mode = False
+
+@app.route("/")
+def hello():
+    return "Coming Soon"
+
+if __name__ == "__main__":
+    if construction_mode:
+        app.debug = True
+    else:
+        app.debug = False
+    socket.run(app, host='127.0.0.1', port=4000, use_reloader=True)
